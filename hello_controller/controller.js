3b75393a5ed7abbdf75f756749cb9c58dbe08719
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-07-20 10:24:06 +0200

    data binding and ng model

--- hello_controller/controller.js
+++ hello_controller/controller.js
@@ -2,4 +2,19 @@ var app = angular.module("MyFirstApp", [])
 
 app.controller("FirstController", function($scope){
     $scope.name = "Fran";
+    $scope.newComment = {};
+    $scope.comments = [
+        {
+            comment: "Good tutorial",
+            username: "codigofacilito"
+        },
+        {
+            comment: "Very bad tutorial",
+            username: "other_user"
+        }
+    ];
+    $scope.addComment = function() {
+        $scope.comments.push($scope.newComment);
+        $scope.newComment = {};
+    }
 });
