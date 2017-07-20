1e4486cfd4509df1817dc38b68869f39906a8bc3
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-07-20 11:12:16 +0200

    AJAX call with POST method

--- http/controller.js
+++ http/controller.js
@@ -2,10 +2,24 @@ var app = angular.module("MyFirstApp", [])
 
 app.controller("FirstController", function($scope, $http) {
     $scope.posts = [];
+    $scope.newPost = {};
     $http.get("http://jsonplaceholder.typicode.com/posts")
-        .then(function(response) {
-            $scope.posts = response.data;
-        }, function(error) {
-
+    .then(function(response) {
+        $scope.posts = response.data;
+    }, function(error) {
+        console.log(error);
+    });
+    $scope.addPost = function() {
+        $http.post("http://jsonplaceholder.typicode.com/posts", {
+            title: $scope.newPost.title,
+            body: $scope.newPost.body,
+            userId: 1
+        })
+        .then(function(data, status, headers, config) {
+            $scope.posts.push($scope.newPost);
+            $scope.newPost = {};
+        }, function(error, status, headers, config) {
+            console.log(error);
         });
+    }
 });
